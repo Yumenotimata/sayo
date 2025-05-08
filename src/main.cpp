72a85f7f1d066df3928b5d9ad107d802f6a5c27a
*null <109791968+Yumenotimata@users.noreply.github.com>
2025-05-08 09:29:11 +0000

    uniquifyの効率化

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <term.hpp>
 
 int main(int argc, char** argv) {
-    auto e1 = let("x0", lambda("x0", var("x0")), app(var("x"), lit(mk_string("Hello"))));
+    auto e1 = let("x0", lambda("x0", var("x0")), app(var("x0"), lit(mk_string("Hello"))));
     try {
         auto u1 = uniquify(std::move(e1));
         std::cout << u1->to_string() << std::endl;
--- src/term.cpp
+++ src/term.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <algorithm>
 #include <stdexcept>
+#include <stack>
 
 std::unique_ptr<expr_t> app(std::unique_ptr<expr_t> e1, std::unique_ptr<expr_t> e2) {
     return std::make_unique<expr_t>(app_t{std::move(e1), std::move(e2)});
@@ -52,13 +53,17 @@ std::string expr_t::to_string() {
 }
 
 using uenv = std::vector<std::tuple<std::string, std::string>>;
+// スコープを破棄せずにpopしたいケースがあるのでstackではなくvectorをスタック的に使う
+using uenv_stack = std::vector<uenv>;
 
 std::string gen_uid(std::string id) {
     static int counter = 0;
     return id + std::to_string(counter++);
 }
 
-std::unique_ptr<expr_t> _uniquify(std::unique_ptr<expr_t> e, uenv uv) {
+// 関数型に毒されすぎてどっちつかずなコードを書いてしまった。暇があったらインプレース化したい
+// 識別子環境を毎回コピーするのだけはさすがに目についたのでインプレース化
+std::unique_ptr<expr_t> _uniquify(std::unique_ptr<expr_t> e, uenv_stack& uv) {
     return std::visit([&uv](auto&& arg) -> std::unique_ptr<expr_t> {
         using T = std::decay_t<decltype(arg)>;
         if constexpr (std::is_same_v<T, app_t>) {
@@ -67,27 +72,30 @@ std::unique_ptr<expr_t> _uniquify(std::unique_ptr<expr_t> e, uenv uv) {
             return app(std::move(e1), std::move(e2));
         } else if constexpr (std::is_same_v<T, lambda_t>) {
             auto ue = gen_uid(arg.e);
-            uenv lex_uv = uv;
-            lex_uv.emplace_back(std::make_tuple(arg.e, ue));
-            auto body = _uniquify(std::move(arg.body), lex_uv);
+            // 関数という構造そのものがスタック的だから、それをスタックの操作に変換できるのは当たり前っちゃ当たり前
+            // ひとつ前のコミットを見ろ
+            uv.push_back(std::vector<std::tuple<std::string, std::string>>{std::make_tuple(arg.e, ue)});
+            auto body = _uniquify(std::move(arg.body), uv);
+            uv.pop_back();
             return lambda(ue, std::move(body));
         } else if constexpr (std::is_same_v<T, let_t>) {
             std::string uid = gen_uid(arg.id);
-            uenv lex_uv = uv;
-            lex_uv.emplace_back(std::make_tuple(arg.id, uid));
-            auto e1 = _uniquify(std::move(arg.e1), lex_uv);
-            auto e2 = _uniquify(std::move(arg.e2), lex_uv);
+            uv.push_back(std::vector<std::tuple<std::string, std::string>>{std::make_tuple(arg.id, uid)});
+            auto e1 = _uniquify(std::move(arg.e1), uv);
+            auto e2 = _uniquify(std::move(arg.e2), uv);
+            uv.pop_back();
             return let(uid, std::move(e1), std::move(e2));
         } else if constexpr (std::is_same_v<T, var_t>) {
             // スコープの階層が近い順に変数を探すために逆順で探査していることに注意
-            auto it = std::find_if(uv.rbegin(), uv.rend(), [&](const auto& pair) {
-                return std::get<0>(pair) == arg.id;
-            });
-            if (it != uv.rend()) {
-                return var(std::get<1>(*it));
-            } else {
-                throw std::runtime_error("Variable not found in environment");
+            for (auto env = uv.rbegin(); env != uv.rend(); ++env) {
+                auto it = std::find_if(env->rbegin(), env->rend(), [&](const auto& pair) {
+                    return std::get<0>(pair) == arg.id;
+                });
+                if (it != env->rend()) {
+                    return var(std::get<1>(*it));
+                }
             }
+            throw std::runtime_error("Variable not found in environment");
         } else if constexpr (std::is_same_v<T, lit_t>) {
             return lit(std::make_unique<value_t>(arg.value->inner));
         }
@@ -95,5 +103,6 @@ std::unique_ptr<expr_t> _uniquify(std::unique_ptr<expr_t> e, uenv uv) {
 }
 
 std::unique_ptr<expr_t> uniquify(std::unique_ptr<expr_t> e) {
-    return _uniquify(std::move(e), {});
+    uenv_stack uv;
+    return _uniquify(std::move(e), uv);
 }
